9a68827a070103192a8f80e64b2ef202dcf5d7a3
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-07-18 22:27:48 +0800

    update base lib and example

--- examples/timer/main.cpp
+++ examples/timer/main.cpp
@@ -8,27 +8,26 @@
 
 using namespace std::chrono_literals;
 
-cppevent::task<void> signal_coroutine(cppevent::event_listener& listener) {
-    for (int i = 1; i <= 5; ++i) {
-        co_await cppevent::read_awaiter { listener };
-        std::cout << "Received an in-app signal: " << i << std::endl;
+cppevent::task<void> signal_coroutine(std::unique_ptr<cppevent::event_listener> listener) {
+    while(true) {
+        co_await cppevent::read_awaiter { *listener };
+        std::cout << "Received an in-app signal" << std::endl;
     }
 }
 
 cppevent::task<void> timed_coroutine(cppevent::event_loop& e_loop) {
-    bool control = true;
-    auto& signal_listener = *(e_loop.get_signal_listener());
-    auto t = signal_coroutine(signal_listener);
-    cppevent::timer timer(3s, e_loop);
-    std::cout << "Starting timed coroutine which triggers every 3 secs 5 times" << std::endl;
-    while (true) {
+    std::unique_ptr<cppevent::event_listener> signal_listener = e_loop.get_signal_listener();
+    auto s_id = signal_listener->get_id();
+    auto t = signal_coroutine(std::move(signal_listener));
+    cppevent::timer timer(2s, e_loop);
+    std::cout << "Starting timed coroutine which triggers every 2 secs 5 times" << std::endl;
+    for (int i = 1; i <= 5; ++i) {
         co_await timer.wait();
-        if (t.m_handle.done()) {
-            break;
-        }
-        std::cout << "Timer triggered" << std::endl;
-        e_loop.send_signal(&signal_listener, true, false);
+        std::cout << "Timer triggered: " << i << std::endl;
+        e_loop.send_signal(s_id, true, false);
     }
+    co_await timer.wait();
+    t.m_handle.destroy();
     e_loop.stop();
 }
 
