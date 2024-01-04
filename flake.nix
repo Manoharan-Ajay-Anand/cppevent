6b7c7e20bbf162b72203ac8eefd16863fc644e12
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-01-04 11:47:59 +0800

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Nix flake for cppevent";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      src = builtins.path {
+        path = ./.;
+        name = "cppevent";
+      };
+      name = "cppevent-1.0";
+      inherit system;
+      nativeBuildInputs = [pkgs.cmake pkgs.liburing];
+    };
+  };
+}
