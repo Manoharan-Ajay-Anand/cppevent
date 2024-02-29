86eec5ecc123cee802c1701dcd3748ea5cf5fd40
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-02-29 21:09:09 +0800

    fix throw error bug for wiat cqe

--- src/cppevent_base/io_service.cpp
+++ src/cppevent_base/io_service.cpp
@@ -21,7 +21,10 @@ std::unique_ptr<cppevent::io_listener> cppevent::io_service::get_listener(int fd
 cppevent::e_event cppevent::io_service::poll() {
     io_uring_cqe* cqe;
     io_uring_submit(&m_ring);
-    throw_if_error(io_uring_wait_cqe(&m_ring, &cqe), "Failed to wait cqe: ");
+    int wait_status = io_uring_wait_cqe(&m_ring, &cqe);
+    if (wait_status < 0) {
+        throw_error("Failed to wait cqe: ", 0 - wait_status);
+    }
     e_id notify_id = io_uring_cqe_get_data64(cqe);
     e_status status = cqe->res;
     io_uring_cqe_seen(&m_ring, cqe);
