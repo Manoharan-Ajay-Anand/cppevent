9027647571c63c218c361b6938fad009b03ea31b
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-03-27 14:34:13 +0800

    update fcgi server

--- src/cppevent_fcgi/fcgi_server.cpp
+++ src/cppevent_fcgi/fcgi_server.cpp
@@ -34,39 +34,35 @@ cppevent::awaitable_task<void> cppevent::fcgi_server::read_req(socket& sock,
     bool close_ctrl = false;
     uint8_t header_data[FCGI_HEADER_LEN];
     uint8_t padding_data[FCGI_MAX_PADDING];
-    try {
-        while (!close_ctrl) {
-            long len_read = co_await sock.read(header_data, FCGI_HEADER_LEN, false);
-            if (len_read != FCGI_HEADER_LEN) {
-                break;
-            }
-            record r = record::parse(header_data);
-            switch (r.m_type) {
-                case FCGI_BEGIN_REQUEST:
-                    {
-                        uint8_t req_data[FCGI_BEGIN_REQ_LEN];
-                        co_await sock.read(req_data, FCGI_BEGIN_REQ_LEN, true);
-                        bool close_conn = (req_data[2] & FCGI_KEEP_CONN) == 0;
-                        requests[r.m_req_id] = std::make_unique<request>(r.m_req_id,
-                                                                         &close_ctrl,
-                                                                         close_conn,
-                                                                         m_loop,
-                                                                         std::ref(sock),
-                                                                         std::ref(control),
-                                                                         std::ref(m_handler));
-                    }
-                    break;
-                case FCGI_STDIN:
-                case FCGI_PARAMS:
-                    co_await requests.at(r.m_req_id)->update(r.m_type, r.m_content_len);
-                    break;
-                default:
-                    throw std::runtime_error("FCGI record type unrecognized");
-            }
-            co_await sock.read(padding_data, r.m_padding_len, true);
+    
+    while (!close_ctrl) {
+        long len_read = co_await sock.read(header_data, FCGI_HEADER_LEN, false);
+        if (len_read != FCGI_HEADER_LEN) {
+            break;
         }
-    } catch (std::runtime_error e) {
-        std::cerr << e.what() << std::endl;
+        record r = record::parse(header_data);
+        switch (r.m_type) {
+            case FCGI_BEGIN_REQUEST:
+                {
+                    uint8_t req_data[FCGI_BEGIN_REQ_LEN];
+                    co_await sock.read(req_data, FCGI_BEGIN_REQ_LEN, true);
+                    bool close_conn = (req_data[2] & FCGI_KEEP_CONN) == 0;
+                    requests[r.m_req_id] = std::make_unique<request>(r.m_req_id,
+                                                                     &close_ctrl, close_conn,
+                                                                     m_loop,
+                                                                     std::ref(sock),
+                                                                     std::ref(control),
+                                                                     std::ref(m_handler));
+                }
+                break;
+            case FCGI_STDIN:
+            case FCGI_PARAMS:
+                co_await requests.at(r.m_req_id)->update(r.m_type, r.m_content_len);
+                break;
+            default:
+                throw std::runtime_error("FCGI record type unrecognized");
+        }
+        co_await sock.read(padding_data, r.m_padding_len, true);
     }
 }
 
@@ -74,6 +70,9 @@ cppevent::task cppevent::fcgi_server::on_connection(std::unique_ptr<socket> sock
     request_map requests;
     output_control control { requests };
     auto write_task = control.begin_res_task(*sock);
-    co_await read_req(*sock, control, requests);
-    // control.shutdown();
+    try {
+        co_await read_req(*sock, control, requests);
+    } catch (std::runtime_error e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
