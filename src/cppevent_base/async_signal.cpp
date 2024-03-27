dab861d102d81a8b3eec64497fe5f98f410e3345
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-03-28 01:05:41 +0800

    update async signal

--- src/cppevent_base/async_signal.cpp
+++ src/cppevent_base/async_signal.cpp
@@ -4,14 +4,39 @@ void cppevent::signal_trigger::activate() {
     m_loop->add_event({ m_id, 0 });
 }
 
+bool cppevent::signal_awaiter::await_ready() {
+    return m_triggered;
+}
+
+void cppevent::signal_awaiter::await_suspend(std::coroutine_handle<> handle) {
+    m_waiting_opt = handle;
+}
+
+void cppevent::signal_awaiter::await_resume() {
+    m_triggered = false;
+}
+
 cppevent::async_signal::async_signal(event_loop& loop): m_loop(loop),
                                                         m_callback(loop.get_event_callback()) {
+    init_handler();
+}
+
+void cppevent::async_signal::init_handler() {
+    m_callback.set_handler([this](e_status status) {
+        init_handler();
+        m_triggered = true;
+        if (m_waiting_opt.has_value()) {
+            auto res_handle = m_waiting_opt.value();
+            m_waiting_opt.reset();
+            res_handle.resume();
+        }
+    });
 }
 
 cppevent::signal_trigger cppevent::async_signal::get_trigger() {
     return { m_callback.get_id(), &m_loop };
 }
 
-cppevent::status_awaiter cppevent::async_signal::await_signal() {
-    return m_callback.await_status();
+cppevent::signal_awaiter cppevent::async_signal::await_signal() {
+    return { m_triggered, m_waiting_opt };
 }
--- src/cppevent_base/async_signal.hpp
+++ src/cppevent_base/async_signal.hpp
@@ -4,6 +4,7 @@
 #include "event_loop.hpp"
 
 #include <coroutine>
+#include <optional>
 
 namespace cppevent {
 
@@ -14,11 +15,26 @@ struct signal_trigger {
     void activate();
 };
 
+struct signal_awaiter {
+    bool& m_triggered;
+    std::optional<std::coroutine_handle<>>& m_waiting_opt;
+
+    bool await_ready();
+
+    void await_suspend(std::coroutine_handle<> handle);
+
+    void await_resume();
+};
+
 class async_signal {
 private:
+    bool m_triggered = false;
+    std::optional<std::coroutine_handle<>> m_waiting_opt;
+
     event_callback m_callback;
     event_loop& m_loop;
 
+    void init_handler();
 public:
     async_signal(event_loop& loop);
 
@@ -29,7 +45,7 @@ public:
     async_signal& operator=(async_signal&&) = delete;
 
     signal_trigger get_trigger();
-    status_awaiter await_signal();
+    signal_awaiter await_signal();
 };
 
 }
