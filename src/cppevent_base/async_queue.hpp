f0ea67ab9f920586072c5875a78198cb48e14771
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-04-09 20:05:19 +0800

    update async_signal and async_queue

--- src/cppevent_base/async_queue.hpp
+++ src/cppevent_base/async_queue.hpp
@@ -10,26 +10,6 @@
 namespace cppevent {
 
 template <typename T>
-struct async_queue_awaiter {
-    std::queue<T>& m_items;
-    event_callback& m_callback;
-
-    bool await_ready() {
-        return !m_items.empty();
-    }
-    
-    void await_suspend(std::coroutine_handle<> handle) {
-        m_callback.set_handler([handle](e_status stat) {
-            handle.resume();
-        });
-    }
-    
-    long await_resume() {
-        return m_items.size();
-    }
-};
-
-template <typename T>
 class async_queue {
 private:
     std::queue<T> m_items;
@@ -39,8 +19,11 @@ public:
     async_queue(event_loop& loop): m_loop(loop), m_callback(loop.get_event_callback()) {
     }
 
-    async_queue_awaiter<T> await_items() {
-        return { m_items, m_callback };
+    awaitable_task<long> await_items() {
+        while (m_items.empty()) {
+            co_await m_callback.await_status();
+        }
+        co_return m_items.size();
     }
 
     T& front() {
--- src/cppevent_base/async_signal.hpp
+++ src/cppevent_base/async_signal.hpp
@@ -15,26 +15,11 @@ struct signal_trigger {
     void activate() const;
 };
 
-struct signal_awaiter {
-    bool& m_triggered;
-    std::optional<std::coroutine_handle<>>& m_waiting_opt;
-
-    bool await_ready();
-
-    void await_suspend(std::coroutine_handle<> handle);
-
-    void await_resume();
-};
-
 class async_signal {
 private:
-    bool m_triggered = false;
-    std::optional<std::coroutine_handle<>> m_waiting_opt;
-
     event_callback m_callback;
     event_loop& m_loop;
 
-    void init_handler();
 public:
     async_signal(event_loop& loop);
 
@@ -45,7 +30,7 @@ public:
     async_signal& operator=(async_signal&&) = delete;
 
     signal_trigger get_trigger();
-    signal_awaiter await_signal();
+    status_awaiter await_signal();
 };
 
 }
