a2410e520e194874aba127f21af7c00b2f7b8f90
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-04-21 22:26:45 +0800

    setup

---
+++ src/cppevent_postgres/connection.hpp
@@ -0,0 +1,12 @@
+#ifndef CPPEVENT_POSTGRES_CONNECTION_HPP
+#define CPPEVENT_POSTGRES_CONNECTION_HPP
+
+namespace cppevent {
+
+class connection {
+
+};
+
+}
+
+#endif
---
+++ src/cppevent_postgres/connection_pool.hpp
@@ -0,0 +1,12 @@
+#ifndef CPPEVENT_POSTGRES_CONNECTION_POOL_HPP
+#define CPPEVENT_POSTGRES_CONNECTION_POOL_HPP
+
+namespace cppevent {
+
+class connection_pool {
+
+};
+
+}
+
+#endif
