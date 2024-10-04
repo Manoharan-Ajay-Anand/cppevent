f58c252fe3007ce36773fdf623c84a4c6b4f89c4
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2024-10-05 00:28:22 +0800

    simplify event loop

--- src/cppevent_base/event_loop.cpp
+++ src/cppevent_base/event_loop.cpp
@@ -10,18 +10,6 @@
 #include <sys/eventfd.h>
 #include <unistd.h>
 
-cppevent::event_loop::event_loop() {
-    m_event_fd_triggered = false;
-    m_running = true;
-    m_event_fd = eventfd(0, 0);
-    throw_if_error(m_event_fd, "Failed to create event fd: ");
-}
-
-cppevent::event_loop::~event_loop() {
-    int status = close(m_event_fd);
-    throw_if_error(status, "Failed to destroy event fd: ");
-}
-
 std::unique_ptr<cppevent::io_listener> cppevent::event_loop::get_io_listener(int fd) {
     return m_io_service.get_listener(fd, m_event_bus);
 }
@@ -30,62 +18,19 @@ cppevent::event_callback cppevent::event_loop::get_event_callback() {
     return event_callback { m_event_bus };
 }
 
-void cppevent::event_loop::trigger_event_fd() {
-    if (!m_event_fd_triggered) {
-        int status = eventfd_write(m_event_fd, 1);
-        throw_if_error(status, "Failed to write to eventfd: ");
-        m_event_fd_triggered = true;
-    }
-}
-
 void cppevent::event_loop::add_event(e_event ev) {
     m_events.push(ev);
-    trigger_event_fd();
-}
-
-void cppevent::event_loop::add_op(const std::function<void()>& op) {
-    m_ops.push(op);
-    trigger_event_fd();
-}
-
-void cppevent::event_loop::run_ops() {
-    std::queue<std::function<void()>> ops = std::move(m_ops);
-    while (!ops.empty() && m_running) {
-        auto op = ops.front();
-        ops.pop();
-        op();
-    }
-}
-
-void cppevent::event_loop::notify_events() {
-    std::queue<e_event> events = std::move(m_events);
-    while (!events.empty() && m_running) {
-        auto event = events.front();
-        events.pop();
-        m_event_bus.notify(event.m_id, event.m_status);
-    }
-}
-
-cppevent::task<> cppevent::event_loop::run_internal_loop() {
-    std::unique_ptr<io_listener> listener = get_io_listener(m_event_fd);
-    uint64_t count;
-    while (m_running) {
-        int status = co_await listener->on_read(&count, 8);
-        if (status >= 0) {
-            m_event_fd_triggered = false;
-            notify_events();
-            run_ops();
-        } else {
-            throw_error("Failed to read from eventfd: ");
-        }
-    }
 }
 
 void cppevent::event_loop::run() {
-    task<> t = run_internal_loop();
     while (m_running) {
-        e_event ev = m_io_service.poll();
-        m_event_bus.notify(ev.m_id, ev.m_status);
+        if (!m_events.empty()) {
+            auto event = m_events.front();
+            m_events.pop();
+            m_event_bus.notify(event.m_id, event.m_status);
+            continue;
+        }
+        add_event(m_io_service.poll());
     }
 }
 
--- src/cppevent_base/event_loop.hpp
+++ src/cppevent_base/event_loop.hpp
@@ -12,32 +12,18 @@ namespace cppevent {
 
 class event_loop {
 private:
-    bool m_event_fd_triggered;
-    bool m_running;
-    int m_event_fd;
+    bool m_running = true;
 
     std::queue<e_event> m_events;
-    std::queue<std::function<void()>> m_ops;
     
     event_bus m_event_bus;
     io_service m_io_service;
 
-    void trigger_event_fd();
-
-    void run_ops();
-    void notify_events();
-    
-    task<> run_internal_loop();
-
 public:
-    event_loop();
-    ~event_loop();
-
     std::unique_ptr<io_listener> get_io_listener(int fd);
     event_callback get_event_callback();
     
     void add_event(e_event ev);
-    void add_op(const std::function<void()>& op);
     
     void run();
     void stop();
